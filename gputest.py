2f223a672e40a2932946bec3a7d575d6c735e632
nevakanezzar <nevakanezzar@gmail.com>
2017-04-03 02:57:00 +0100

    gputest

--- gputest.py
+++ gputest.py
@@ -4,4 +4,4 @@ def get_available_devices():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos]
 
-get_available_devices()
+print(get_available_devices())
