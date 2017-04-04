e1295f39d5564947ae8e34e18e0e508031d0c837
nevakanezzar <nevakanezzar@gmail.com>
2017-04-04 07:46:13 +0100

    scripts

---
+++ b20.1500000.sh
@@ -0,0 +1,9 @@
+#$ -l tmem=1G
+#$ -l h_vmem=1G
+#$ -l h_rt=12:00:00
+#$ -S /bin/bash
+#$ -j y
+#$ -wd $HOME/git/atml_a3
+hostname
+date
+python3 ~/git/atml_a3/b2.py 2 0.1 500000
---
+++ gen_scripts.py
@@ -0,0 +1,30 @@
+
+games = ['0','1','2']
+learning_rates = ['0.1','0.01','0.001','0.0001','0.00001']
+discount_factor = ['0','500000']
+
+files = []
+
+for g in games:
+	for l in learning_rates:
+		for d in discount_factor:
+			filename = "b"+g+l+d+".sh"
+			text = ["#$ -l tmem=1G\n",
+					"#$ -l h_vmem=1G\n",
+					"#$ -l h_rt=12:00:00\n",
+					"#$ -S /bin/bash\n",
+					"#$ -j y\n",
+					"#$ -wd $HOME/git/atml_a3\n"
+					"hostname\n",
+					"date\n",
+					"python3 ~/git/atml_a3/b2.py "+g+" "+l+" "+d+"\n"]
+			with open(filename,'w') as f:
+				f.write("".join(text))	
+			files.append(filename)
+
+master = "masterscript.sh"
+
+with open(master,'w') as f:
+	for f1 in files:
+		f.write("qsub "+f1+"\n")
+
