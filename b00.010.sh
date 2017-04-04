14274fda6e8ac81e8408d6298c558a573c8e25a6
nevakanezzar <nevakanezzar@gmail.com>
2017-04-04 07:58:27 +0100

    scripts

--- b00.010.sh
+++ b00.010.sh
@@ -3,7 +3,7 @@
 #$ -l h_rt=12:00:00
 #$ -S /bin/bash
 #$ -j y
-#$ -wd $HOME/git/atml_a3/
+#$ -wd /home/skasewa/git/atml_a3/
 hostname
 date
 python3 ~/git/atml_a3/b2.py 0 0.01 0
--- b10.001500000.sh
+++ b10.001500000.sh
@@ -3,7 +3,7 @@
 #$ -l h_rt=12:00:00
 #$ -S /bin/bash
 #$ -j y
-#$ -wd $HOME/git/atml_a3/
+#$ -wd /home/skasewa/git/atml_a3/
 hostname
 date
 python3 ~/git/atml_a3/b2.py 1 0.001 500000
