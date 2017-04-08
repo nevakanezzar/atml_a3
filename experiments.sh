eff2af3ae7a97458435e2b1ffc6f078600503af3
nevakanezzar <nevakanezzar@gmail.com>
2017-04-08 16:55:08 +0100

    refresh

---
+++ experiments.sh
@@ -0,0 +1,12 @@
+python b34_3.py 0 0.01 0 |& tee results.a0.0.01.0
+python b34_3.py 1 0.01 0 |& tee results.b0.0.01.1
+python b34_3.py 2 0.01 0 |& tee results.c0.0.01.2
+
+python b34_3.py 0 0.001 0 |& tee results.d0.0.001.0
+python b34_3.py 1 0.001 0 |& tee results.e0.0.001.1
+python b34_3.py 2 0.001 0 |& tee results.f0.0.001.2
+
+python b34_3.py 0 0.0001 0 |& tee results.g0.0.0001.0
+python b34_3.py 1 0.0001 0 |& tee results.h0.0.0001.1
+python b34_3.py 2 0.0001 0 |& tee results.i0.0.0001.2
+
